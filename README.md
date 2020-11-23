7ea84b9fe9f322f78d76f865c34b997125738cae
NicoCPH <cph-na157@cphbusiness.dk>
2020-11-23 12:16:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# r9yt7m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/r9yt7m)
---
+++ index.ts
@@ -0,0 +1,35 @@
+import { enableRipple } from '@syncfusion/ej2-base';
+enableRipple(true);
+
+import { Schedule, Day, Week, WorkWeek, Month, Agenda, EventRenderedArgs, Resize, DragAndDrop } from '@syncfusion/ej2-schedule';
+import * as dataSource from './datasource.json';
+import { extend } from '@syncfusion/ej2-base';
+
+Schedule.Inject(Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop);
+
+/**
+ * Schedule local data sample
+ */
+
+
+    
+    let data: Object[] = <Object[]>extend([], (dataSource as any).zooEventsData, null, true);
+    let scheduleObj: Schedule = new Schedule({
+        width: '100%',
+        height: '650px',
+        selectedDate: new Date(2018, 1, 15),
+        eventSettings: { dataSource: data },
+        eventRendered: (args: EventRenderedArgs) => {
+            let categoryColor: string = args.data.CategoryColor as string;
+            if (!args.element || !categoryColor) {
+                return;
+            }
+            if (scheduleObj.currentView === 'Agenda') {
+                (args.element.firstChild as HTMLElement).style.borderLeftColor = categoryColor;
+            } else {
+                args.element.style.backgroundColor = categoryColor;
+            }
+        },
+    });
+    scheduleObj.appendTo('#Schedule');
+
